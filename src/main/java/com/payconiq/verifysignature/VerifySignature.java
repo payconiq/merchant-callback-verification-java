94089f3606025d73173b60226678d2f5ff642ccb
payconiq-devsupport <48285050+payconiq-devsupport@users.noreply.github.com>
2019-09-17 19:58:31 +0200

    Updated verify signature
    Updated verify signature

--- src/main/java/com/payconiq/verifysignature/VerifySignature.java
+++ src/main/java/com/payconiq/verifysignature/VerifySignature.java
@@ -14,11 +14,14 @@ import java.util.zip.CRC32;
 
 public class VerifySignature {
     public static void main(String[] args) {
-        String signature = "IrJO4pIA8mhRKAHM7731FmZNDfBIcdce/4H9N22mldZZEWgfLcolc6xEIaEtHvCcZ1HVdL1KKeXp10FdwAq/3NVHY1RkoBMstUtdQQ9UgKnFovIkGhYm3u7N5G9YM7iivX3AOMiBSCBgTAVsan731e57LTc3Q1klWlSJwyqdwncYYvXv41Aov0UpnzE0UWgmJjmffbezHZOxiWP/dXZV5sCkYSvK5oe0WbLGsJzR8yFuByNCfrv6NDN6V45YvnicXJ1+CPnuR6cEngCxmQEYE0K668IaD3B2zblLiTb89b23ft/E8LaUsM2iNEgI7f1LuL7FzNW09KrPYgDFPFurHA==";
-        String merchantId = "5981e8a4c9716c3dca30679d";
-        String timestamp = "2017-08-30T13:57:48.726Z";
-        String body = "{\"_id\":\"59a6c431f9285003bd46fe56\",\"status\":\"SUCCEEDED\"}";
+        
+        String signature = "RwdPmsA63xbTIa0MWDK3MOnibYfiObZAuZlJ/skvHenP6jSi5+UZSBG+NewwtoWQTnNV4BNHAtm65l6lX1/X1b7zdvavDipJyFr4fh/WKZmPV1g4RJfdMJALNgTO3E7Jp1uI2sji2fBR0+1FikOH2I32aZhCPqzTFP+jBY+18/pR6ZWq8n2TwdS12XLZPCo1Y6r+4VXmxuY0gzxzQ7ozmFbgES6E10mfXAL54Uk1IJ89vvYeV3PHK2VG0oYVzmoTWj4NLpflB3dJcVjG5k0DeGhwyovCPGIMtzxTCSKco7yIFwEh0c3/IXKPUcyMaJzQi3GwEPWC10uctngWDxt3/Q==";
+        String merchantId = "569f8289c25c4103a3597ec8";
+        String timestamp = "2019-04-23T12:24:57.451Z";
+        String body = "{\"_id\":\"ddfc15cb007c5449082459dd\",\"status\":\"SUCCEEDED\",\"externalRefId\":\"1556021853839\"}";
         String alg = "SHA256WithRSA";
+
+        
         String env = "dev";
         boolean isValid = isValidSignature(signature, merchantId, timestamp, body, alg, env);
 
